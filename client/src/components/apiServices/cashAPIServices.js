d9a1607c20ec51e5a863d92b57a0d65d88273516
syednoor058 <syednoor058@gmail.com>
2025-03-12 23:40:49 +0600

    salary and cash supply added! (2)

--- client/src/components/apiServices/cashAPIServices.js
+++ client/src/components/apiServices/cashAPIServices.js
@@ -23,3 +23,28 @@ const getHeaders = () => {
       throw error.response?.data || { message: 'Failed to fetch cash opening.' };
     }
   }
+
+  export const getSupplyCashHistory = async (formattedDate) => {
+    try{
+      const response = await axios.get(
+        `${API_BASE_URL}/api/supply_cash_project/history/${formattedDate}`,
+        {headers: getHeaders(),}
+      );
+      // console.log(response)
+      return response.data;
+    } catch(error) {
+      throw error.response?.data || { message: 'Failed to fetch cash history.' };
+    }
+  }
+  export const supplyProjectCash = async (project_id, cash_amount) => {
+    try{
+      const response = await axios.post(
+        `${API_BASE_URL}/api/supply_cash_project`, {project_id, cash_amount},
+        {headers: getHeaders(),}
+      );
+      // console.log(response)
+      return response.data;
+    } catch(error) {
+      throw error.response?.data || { message: 'Failed to supply cash.' };
+    }
+  }
