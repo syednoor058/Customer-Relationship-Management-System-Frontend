441cb2564c56740134776eb034c4f8b1fe31cbf4
Mazharul <imazharul1101@gmail.com>
2025-03-12 12:54:04 +0600

    add api for opening cash on cashAPIServices

--- client/src/components/apiServices/cashAPIServices.js
+++ client/src/components/apiServices/cashAPIServices.js
@@ -4,23 +4,29 @@ const API_BASE_URL = import.meta.env.VITE_SIKDER_CMS_APP_API;
 
 // Add default headers (e.g., Authorization token)
 const getHeaders = () => {
-    const token = localStorage.getItem('shikderFoundationAuthToken'); // Store your token in local storage or another secure place
-    return {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    };
+  const token = localStorage.getItem('shikderFoundationAuthToken'); // Store your token securely
+  return {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
   };
+};
 
+export const getCash = async () => {
+  try {
+    const openingResponse = await axios.get(`${API_BASE_URL}/api/cash/opening`, {
+      headers: getHeaders(),
+    });
 
-  export const getCash = async () => {
-    try{
-      const response = await axios.get(
-        `${API_BASE_URL}/api/cash`,
-        {headers: getHeaders(),}
-      );
-      // console.log(response)
-      return response.data;
-    } catch(error) {
-      throw error.response?.data || { message: 'Failed to fetch cash opening.' };
+    if (openingResponse.data.success) {
+      const cashResponse = await axios.get(`${API_BASE_URL}/api/cash`, {
+        headers: getHeaders(),
+      });
+
+      return cashResponse.data; 
     }
+
+    return [{ balance: 0, updated_at: null }];
+  } catch (error) {
+    throw error.response?.data || { message: 'Failed to fetch cash opening.' };
   }
+};
